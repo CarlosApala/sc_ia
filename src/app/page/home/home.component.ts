72e1719d6f4a3c519508325f30141fcc0c7557bd
Jose Carlos Apala M <53344166+CarlosApala@users.noreply.github.com>
2024-07-27 15:56:18 -0400

    se añadio pequeñas mejoras

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -9,6 +9,16 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class HomeComponent {
   private navbar!: HTMLElement;
 
+  listIcon: String[] = [
+    'assets/img/icon/chatgpt.svg',
+    'assets/img/icon/copy.ai.svg',
+    'assets/img/icon/ibm-watson.svg',
+    'assets/img/icon/jasper-ai.svg',
+    'assets/img/icon/microsoft-copilot.svg',
+    'assets/img/icon/Midjourney.png',
+    'assets/img/icon/power-bi1.svg',
+  ];
+
   constructor() {}
 
   logoUrl = './assets/img/new_logo.jpg';
